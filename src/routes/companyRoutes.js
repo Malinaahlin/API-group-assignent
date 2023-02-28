4de121066f2cf8ec79b0463c98da7f7314641ba1
maxtkarlsson <max.karlsson93@gmail.com>
2023-02-28 16:04:51 +0100

    route scaffolding

---
+++ src/routes/companyRoutes.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+
+// GET - /api/v1/companies
+
+// GET - /api/v1/companies/:companyId
+
+// POST - /api/v1/companies
+
+// PUT - /api/v1/companies/:companyId
+
+// DELETE - /api/v1/companies/:companyId
+
+module.exports = router;
