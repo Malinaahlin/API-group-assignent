64ed4ca70e0c965422e861b895cbbbcbee8ef37a
maxtkarlsson <max.karlsson93@gmail.com>
2023-02-28 15:34:47 +0100

    scaffolding routes

---
+++ src/routes/workshopRoutes.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+
+// GET - /api/v1/workshops
+
+// GET - /api/v1/workshops/:workshopId
+
+// POST - /api/v1/workshops
+
+// PUT - /api/v1/workshops/:workshopId
+
+// DELETE - /api/v1/workshops/:workshopId
+
+module.exports = router;
