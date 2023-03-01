44a2c1813d60d5a93e35a6a268b83c85d7adfb58
maxtkarlsson <max.karlsson93@gmail.com>
2023-03-01 16:13:44 +0100

    userRoutes tested

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,8 +1,33 @@
-const { users } = require("../constants/users")
-const { NotFoundError, UnauthorizedError } = require("../utils/errors")
-const { sequelize } = require("../database/config")
-const { QueryTypes } = require("sequelize")
+const { users } = require("../constants/users");
+const { NotFoundError, UnauthorizedError } = require("../utils/errors");
+const { sequelize } = require("../database/config");
+const { QueryTypes } = require("sequelize");
 
+// GET - /api/v1/users
+exports.getAllUsers = async (req, res) => {
+  return res.send("getAllUsers has been called.");
+};
+
+// GET - /api/v1/users/:userId
+exports.getUserById = async (req, res) => {
+  return res.send("getUserById has been called.");
+};
+
+// POST - /api/v1/users
+exports.createNewUser = async (req, res) => {
+  return res.send("createNewUser has been called.");
+};
+
+// PUT - /api/v1/users/:userId
+exports.updateUserById = async (req, res) => {
+  return res.send("updateUserById has been called.");
+};
+
+// DELETE - /api/v1/users/:userId
+exports.deleteUserById = async (req, res) => {
+  return res.send("deleteUserById has been called.");
+};
+/*
 exports.getAllUsers = async (req, res) => {
 	const [users, metadata] = await sequelize.query("SELECT id, email FROM users")
 	return res.json(users)
@@ -39,3 +64,4 @@ exports.deleteUserById = async (req, res) => {
 
 	return res.sendStatus(204)
 }
+*/
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,21 +1,28 @@
 const express = require("express");
 const router = express.Router();
-const { users } = require("../constants/users");
+//const { users } = require("../constants/users");
 const {
   getAllUsers,
   getUserById,
+  createNewUser,
+  updateUserById,
   deleteUserById,
 } = require("../controllers/userControllers");
 //const { isAuthenticated, authorizeAccontType } = require('../middleware/authenticationMiddleware')
 
 // GET - /api/v1/users
+router.get("/", getAllUsers);
 
 // GET - /api/v1/users/:userId
+router.get("/:userId", getUserById);
 
 // POST - /api/v1/users
+router.post("/", createNewUser);
 
 // PUT - /api/v1/users/:userId
+router.put("/:userId", updateUserById);
 
 // DELETE - /api/v1/users/:userId
+router.delete("/:userId", deleteUserById);
 
 module.exports = router;
