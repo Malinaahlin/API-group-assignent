95492445fe3c3a0daa11d7630a63f6eb9ed86574
maxtkarlsson <max.karlsson93@gmail.com>
2023-03-01 15:21:59 +0100

    cityRoutes tested

--- src/routes/cityRoutes.js
+++ src/routes/cityRoutes.js
@@ -1,14 +1,26 @@
 const express = require("express");
 const router = express.Router();
+const {
+  getAllCities,
+  getCityById,
+  createNewCity,
+  updateCityById,
+  deleteCityById,
+} = require("../controllers/cityControllers");
 
 // GET - /api/v1/cities
+router.get("/", getAllCities);
 
 // GET - /api/v1/cities/:cityId
+router.get("/:cityId", getCityById);
 
 // POST - /api/v1/cities
+router.post("/", createNewCity);
 
 // PUT - /api/v1/cities/:cityId
+router.put("/:cityId", updateCityById);
 
 // DELETE - /api/v1/cities/:cityId
+router.delete("/:cityId", deleteCityById);
 
 module.exports = router;
