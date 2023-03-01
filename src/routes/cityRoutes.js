4a2acfb9c005c927f2242dad8b1360d1dc5970b0
maxtkarlsson <max.karlsson93@gmail.com>
2023-03-01 11:25:41 +0100

    scaffolding routes

---
+++ src/routes/cityRoutes.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+
+// GET - /api/v1/cities
+
+// GET - /api/v1/cities/:cityId
+
+// POST - /api/v1/cities
+
+// PUT - /api/v1/cities/:cityId
+
+// DELETE - /api/v1/cities/:cityId
+
+module.exports = router;
