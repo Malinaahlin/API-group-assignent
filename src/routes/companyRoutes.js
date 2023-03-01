7011261d7c3d3426bff517fac2088b281f9bcb16
maxtkarlsson <max.karlsson93@gmail.com>
2023-03-01 13:54:13 +0100

    company routes tested

--- src/routes/companyRoutes.js
+++ src/routes/companyRoutes.js
@@ -1,14 +1,26 @@
 const express = require("express");
 const router = express.Router();
+const {
+  getAllCompanies,
+  getCompanyById,
+  createNewCompany,
+  updateCompanyById,
+  deleteCompanyById,
+} = require("../controllers/companyControllers");
 
 // GET - /api/v1/companies
+router.get("/", getAllCompanies);
 
 // GET - /api/v1/companies/:companyId
+router.get("/:companyId", getCompanyById);
 
 // POST - /api/v1/companies
+router.post("/", createNewCompany);
 
 // PUT - /api/v1/companies/:companyId
+router.put("/:companyId", updateCompanyById);
 
 // DELETE - /api/v1/companies/:companyId
+router.delete("/:companyId", deleteCompanyById);
 
 module.exports = router;
