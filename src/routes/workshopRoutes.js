53924cdf903026293452a4df2552777ffd6f4151
maxtkarlsson <max.karlsson93@gmail.com>
2023-03-02 15:39:24 +0100

    workshop routes and controllers scaffolded + teste

--- src/routes/workshopRoutes.js
+++ src/routes/workshopRoutes.js
@@ -1,14 +1,26 @@
 const express = require("express");
 const router = express.Router();
+const {
+  getAllWorkshops,
+  getWorkshopById,
+  createNewWorkshop,
+  updateWorkshopById,
+  deleteWorkshopById,
+} = require("../controllers/workshopControllers");
 
 // GET - /api/v1/workshops
+router.get("/", getAllWorkshops);
 
 // GET - /api/v1/workshops/:workshopId
+router.get("/:workshopId", getWorkshopById);
 
 // POST - /api/v1/workshops
+router.post("/", createNewWorkshop);
 
 // PUT - /api/v1/workshops/:workshopId
+router.put("/:workshopId", updateWorkshopById);
 
 // DELETE - /api/v1/workshops/:workshopId
+router.delete("/:workshopId", deleteWorkshopById);
 
 module.exports = router;
