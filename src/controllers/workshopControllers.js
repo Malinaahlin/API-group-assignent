72ad008ccdd2578d5728e918438b9114fd89c5a4
maxtkarlsson <max.karlsson93@gmail.com>
2023-03-06 13:48:08 +0100

    working on controller getWorkshopById

--- src/controllers/workshopControllers.js
+++ src/controllers/workshopControllers.js
@@ -1,3 +1,9 @@
+const { users, accountType, userRoles } = require("../constants/users");
+const { NotFoundError, UnauthorizedError } = require("../utils/errors");
+const { sequelize } = require("../database/config");
+const { QueryTypes } = require("sequelize");
+const { account_types } = require("../data/account_types");
+
 // GET - /api/v1/workshops
 exports.getAllWorkshops = async (req, res) => {
   return res.send("getAllWorkshops has been called");
@@ -5,7 +11,23 @@ exports.getAllWorkshops = async (req, res) => {
 
 // GET - /api/v1/workshops/:workshopId
 exports.getWorkshopById = async (req, res) => {
-  return res.send("getWorkshopById has been called");
+  // return res.send("getWorkshopById has been called");
+
+  const workshopId = req.params.workshopId;
+
+  const [workshop, metadata] = await sequelize.query(
+    `SELECT * 
+    FROM workshop 
+    WHERE workshop_id = $workshopId;`,
+    {
+      bind: { workshopId },
+      type: QueryTypes.SELECT,
+    }
+  );
+
+  if (!workshop) throw new NotFoundError("That workshop does not exist");
+
+  return res.json(workshop);
 };
 
 // POST - /api/v1/workshops
