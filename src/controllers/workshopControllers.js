1c57da1cabec012e0d573394175e8bdbab52584f
maxtkarlsson <max.karlsson93@gmail.com>
2023-03-07 11:00:59 +0100

    workshop controller getWorksShopById query

--- src/controllers/workshopControllers.js
+++ src/controllers/workshopControllers.js
@@ -15,12 +15,18 @@ exports.getWorkshopById = async (req, res) => {
 
   const workshopId = req.params.workshopId;
 
-  const [workshop, metadata] = await sequelize.query(
-    `SELECT w.name, r.content
+  const workshop = await sequelize.query(
+    `SELECT w.name AS workshop, c.name AS city, r.content AS review, r.rating, u.username 
     FROM workshop w
     LEFT JOIN review r
-    ON r.fk_workshop_id = $workshopId;`,
+    ON r.fk_workshop_id = w.workshop_id
+    LEFT JOIN user u 
+    ON u.user_id = r.fk_user_id 
+    LEFT JOIN city c 
+    ON c.city_id = w.fk_city_id 
+    WHERE w.workshop_id = $workshopId;`,
     // SKA VI HA MED USER MED SÅ MAN SER AUTHOR? - JA
+    //Gör en map som gör det vi vill.
     {
       bind: { workshopId },
       type: QueryTypes.SELECT,
@@ -28,6 +34,13 @@ exports.getWorkshopById = async (req, res) => {
   );
 
   if (!workshop) throw new NotFoundError("That workshop does not exist");
+  /*
+    const response = {
+    workshop: store,
+    reviews: reviews,
+  };
+
+  */
 
   return res.json(workshop);
 };
