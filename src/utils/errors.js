755abedfdddf6b365a4bb01bfedcf5572bcd3136
Mira <miravitsas1@gmail.com>
2023-03-07 10:57:46 +0100

    updateuser done

--- src/utils/errors.js
+++ src/utils/errors.js
@@ -1,44 +1,52 @@
 const { fn } = require("sequelize");
 
 const catchErrors = (fn) => {
-    return function (req, res, next) {
-        return fn(req, res, next).catch(next)
-    }
-}
+  return function (req, res, next) {
+    return fn(req, res, next).catch(next);
+  };
+};
 
 class CustomAPIError extends Error {
-    constructor(message) {
-        super(message)
-    }
+  constructor(message) {
+    super(message);
+  }
+}
+class BadRequestError extends CustomAPIError {
+  constructor(message) {
+    super(message);
+    this.statusCode = 400;
+    this.name = "BadRequestError";
+  }
 }
 
 class NotFoundError extends CustomAPIError {
-    constructor(message) {
-        super(message)
-        this.statusCode = 404
-        this.name = "NotFoundError"
-    }
+  constructor(message) {
+    super(message);
+    this.statusCode = 404;
+    this.name = "NotFoundError";
+  }
 }
 
 class UnauthorizedError extends CustomAPIError {
-    constructor(message) {
-        super(message)
-        this.statusCode = 403
-        this.name = "UnauthorizedError"
-    }
+  constructor(message) {
+    super(message);
+    this.statusCode = 403;
+    this.name = "UnauthorizedError";
+  }
 }
 
 class UnauthenticatedError extends CustomAPIError {
-    constructor(message) {
-        super(message)
-        this.statusCode = 401
-        this.name = "UnauthenticatedError"
-    }
+  constructor(message) {
+    super(message);
+    this.statusCode = 401;
+    this.name = "UnauthenticatedError";
+  }
 }
 
 module.exports = {
-    catchErrors,
-    NotFoundError,
-    UnauthorizedError,
-    UnauthenticatedError
-}
+  catchErrors,
+  BadRequestError,
+  NotFoundError,
+  UnauthorizedError,
+  UnauthenticatedError,
+};
